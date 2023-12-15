396addfbba0c581eb0fbb215c6e1077ba907ffa3
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-15 00:19:32 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const parseCode = (str) => {
-  // your code here
+  let user = {
+    firstName: '',
+    lastName: '',
+    id: ''
+  };
+  let val = '';
+  let property = '';
+
+  for (let key of str) {
+    if (key !== '0') {
+      val += key;
+    } else {
+      if (!property) {
+        property = 'firstName';
+      } else if (property === 'firstName') {
+        user.firstName = val;
+        property = 'lastName';
+        val = '';
+      } else if (property === 'lastName') {
+        user.lastName = val;
+        property = 'id';
+        val = '';
+      }
+    }
+  }
+
+  if (property === 'id') {
+    user.id = val;
+  }
+
+  return user;
 };
+
 
 // Do not change the code below
 const str = prompt("Enter str: ");
